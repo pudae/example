a2aff29e5771e86b9f028d70e96de38690bc215d
pudae <pudae81@gmail.com>
2014-06-05 15:36:07 +0900

    add result sample

---
+++ thrift_tool/message_type.thrift
@@ -0,0 +1,8 @@
+namespace * test.protocol.room
+enum MessageType
+{
+	kReqJoin	= 16265848316414058378
+	kReqLeave	= 13861186144209366286
+	kAnsJoin	= 12741241207097871586
+	kAnsLeave	= 15412690476138873038
+}
